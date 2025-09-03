6fc5b5d757675fa6890000d4ff88bc77942f76b7
Condrey <wangzdrey@gmail.com>
2025-09-03 23:19:34 +0300

    loading indicator

---
+++ app/api/loading.tsx
@@ -0,0 +1,10 @@
+import { Loader2Icon } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <div className="flex h-full min-h-dvh w-full items-center justify-center">
+      <p className="text-muted-foreground">Loading...</p>
+      <Loader2Icon className="animate-spin" />
+    </div>
+  );
+}
