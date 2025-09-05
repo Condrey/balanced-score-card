2c16d9ebd3d76896fb10087aad2c789220f207d1
Condrey <wangzdrey@gmail.com>
2025-09-05 19:52:03 +0300

    deng

--- app/api/form/initial/route.ts
+++ app/api/form/initial/route.ts
@@ -6,54 +6,49 @@ import { bscSchema } from "@/lib/validations/bsc";
 // NDP programmes for a supervisee
 // There strategic objectives
 // Performance objectives
-export async function GET(req: Request, res: Response) {
-  try {
-    const body = await req.json();
-    const { supervisee, year, organizationId } = bscSchema.parse(body);
-    const [organizationContext, position] = await Promise.all([
-      await prisma.organizationContext.findFirst({
-        where: { organizationId, financialYear: year },
-        include: organizationContextDataInclude,
-      }),
-      await prisma.position.findFirst({ where: { id: supervisee.id } }),
-    ]);
-    if (!organizationContext) {
-      const errorMessage =
-        "Failed to get Organization contexts for this year and organization";
-      return Response.json(errorMessage, {
-        status: 200,
-        statusText: errorMessage,
-      });
-    }
-    if (!position) {
-      const errorMessage =
-        "The position you entered has missing info in the database, please cross check and submit again";
-      return Response.json(errorMessage, {
-        status: 200,
-        statusText: errorMessage,
-      });
-    }
-    const { ndp } = organizationContext;
-    if (!ndp) {
-      const errorMessage =
-        "Your organization is missing NDP for the given financial year";
-      return Response.json(errorMessage, {
-        status: 200,
-        statusText: errorMessage,
-      });
-    }
-    const { osps: ndpOsps, programmes: ndpProgrammes } = ndp;
-    const { duties, departmentalMandate } = position;
-    return Response.json(
-      { ndp, position },
-      { statusText: "Success", status: 200 },
-    );
-  } catch (e) {
-    console.error(e);
-    const errorMessage = "There was a server error, please try again.";
-    return Response.json(errorMessage, {
-      status: 500,
-      statusText: errorMessage,
-    });
-  }
+export async function GET(req: Request) {
+	try {
+		const body = await req.json();
+		const { supervisee, year, organizationId } = bscSchema.parse(body);
+		const [organizationContext, position] = await Promise.all([
+			await prisma.organizationContext.findFirst({
+				where: { organizationId, financialYear: year },
+				include: organizationContextDataInclude
+			}),
+			await prisma.position.findFirst({ where: { id: supervisee.id } })
+		]);
+		if (!organizationContext) {
+			const errorMessage = "Failed to get Organization contexts for this year and organization";
+			return Response.json(errorMessage, {
+				status: 200,
+				statusText: errorMessage
+			});
+		}
+		if (!position) {
+			const errorMessage =
+				"The position you entered has missing info in the database, please cross check and submit again";
+			return Response.json(errorMessage, {
+				status: 200,
+				statusText: errorMessage
+			});
+		}
+		const { ndp } = organizationContext;
+		if (!ndp) {
+			const errorMessage = "Your organization is missing NDP for the given financial year";
+			return Response.json(errorMessage, {
+				status: 200,
+				statusText: errorMessage
+			});
+		}
+		const { osps: ndpOsps, programmes: ndpProgrammes } = ndp;
+		const { duties, departmentalMandate } = position;
+		return Response.json({ ndp, position }, { statusText: "Success", status: 200 });
+	} catch (e) {
+		console.error(e);
+		const errorMessage = "There was a server error, please try again.";
+		return Response.json(errorMessage, {
+			status: 500,
+			statusText: errorMessage
+		});
+	}
 }
