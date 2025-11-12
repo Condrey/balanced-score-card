1f90c58e54ee02965fae8d110728f481b4a6e8cd
Condrey <wangzdrey@gmail.com>
2025-11-12 21:45:09 +0300

    paddings

--- app/(home)/add-bsc/page.tsx
+++ app/(home)/add-bsc/page.tsx
@@ -1,10 +1,10 @@
 import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import prisma from "@/lib/prisma";
+import { positionDataInclude } from "@/lib/types";
 import { verifySession } from "@/lib/verify-session";
 import { Metadata } from "next";
 import PageContainer from "../page-container";
 import FormAddBalancedScoreCard from "./form-add-balanced-score-card";
-import prisma from "@/lib/prisma";
-import { positionDataInclude } from "@/lib/types";
 
 export const metadata: Metadata = {
 	title: "Add Balanced Score Card",
@@ -13,13 +13,16 @@ export const metadata: Metadata = {
 
 export default async function Page() {
 	const { session } = await verifySession();
-    const positions = await prisma.position.findMany({include:positionDataInclude})
+	const positions = await prisma.position.findMany({ include: positionDataInclude });
 	return (
-		<PageContainer heading={[{label:'Home',url:'/'},{ label: "Add Balanced Score Card" }]} className="max-w-5xl w-full mx-auto">
-			<CardHeader className="bg-background">
+		<PageContainer
+			heading={[{ label: "Home", url: "/" }, { label: "Add Balanced Score Card" }]}
+			className="max-w-5xl w-full mx-auto"
+		>
+			<CardHeader className="bg-background px-0">
 				<CardTitle className="uppercase">Create your new Balanced Score Card</CardTitle>
 			</CardHeader>
-			<CardContent>
+			<CardContent className="px-0">
 				<FormAddBalancedScoreCard user={session.user} positions={positions} />
 			</CardContent>
 		</PageContainer>
